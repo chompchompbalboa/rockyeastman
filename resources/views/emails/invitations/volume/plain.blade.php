1a460725488536dd5008f715b264be86b2ff878e
Rocky Eastman Jr <eastmanrjr@gmail.com>
2018-07-09 16:54:36 -0700

    Updated volume template

--- resources/views/emails/invitations/volume/plain.blade.php
+++ resources/views/emails/invitations/volume/plain.blade.php
@@ -4,11 +4,9 @@ I hope you’re having an excellent {{ $day }}.
 \r\n\r\n
 I’m reaching out today because I noticed your firm's website was a little out of date. I find businesses with older websites and offer to modernize them.
 \r\n\r\n
-I have a starting point that I think would look great for your firm - you can find it here:
+Could I propose a redesign for your home page and show you what an updated site might look like? I generally use your existing site as a starting point for branding, but am also happy to include anything you might have input on. I want to stay true to the reputation you've already built.
 \r\n\r\n
-http://rockyeastman.com/preview/{{ $slug }}
-\r\n\r\n
-If you like what you see - or are interested in a new site but not the aesthetic above - I'm happy to get started working on a proposal as soon as I hear back from you. Please let me know!
+Let me know - I'm eager to help improve your online presence.
 \r\n\r\n
 Best regards,
 \r\n
