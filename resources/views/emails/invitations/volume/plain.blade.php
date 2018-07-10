4cd1b7da0d143469b18be3e05f4ea5620d485835
Rocky Eastman Jr <eastmanrjr@gmail.com>
2018-07-09 17:36:09 -0700

    ... and yet more template changes

--- resources/views/emails/invitations/volume/plain.blade.php
+++ resources/views/emails/invitations/volume/plain.blade.php
@@ -4,7 +4,9 @@ I hope you’re having an excellent {{ $day }}.
 \r\n\r\n
 I’m reaching out today because I noticed your firm's website was a little out of date.
 \r\n\r\n
-Could I propose a redesign for your home page and show you what an updated site might look like? I generally use your existing site as a starting point for branding, but am also happy to include anything you might have input on. I want to stay true to the reputation you've already built.
+Could I propose a redesign for your home page and show you what an updated site might look like? My plan is to use your existing site as a starting point for branding, but am also happy to include anything you might have input on. I definitely want to stay true to the reputation you've already built.
+\r\n\r\n
+Let me know, I'm always happy to help.
 \r\n\r\n
 Best regards,
 \r\n
