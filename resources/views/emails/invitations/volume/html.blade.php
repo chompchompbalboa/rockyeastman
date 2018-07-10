fe5245d96001a5e99e3cf95a635a91bb824b1088
Rocky Eastman Jr <eastmanrjr@gmail.com>
2018-07-09 23:55:53 -0700

    Template changes

--- resources/views/emails/invitations/volume/html.blade.php
+++ resources/views/emails/invitations/volume/html.blade.php
@@ -7,9 +7,9 @@
   <br/><br/>
   I’m reaching out today because I noticed your firm's website was a little out of date.
   <br/><br/>
-  Could I propose a redesign for your home page and show you what an updated site might look like? My plan is to use your existing site as a starting point for branding, but am also happy to include anything you might have input on. I definitely want to stay true to the reputation you've already built.
+  Could I propose a redesign for your home page and show you what an updated site might look like? 
   <br/><br/>
-  Let me know, I'm always happy to help.
+  My plan is to use your existing site as a starting point for branding, but I am also happy to include anything you might have input on. I definitely want to stay true to the reputation you've already built.
   <br/><br/>
   Best regards,
   <br/>
--- resources/views/emails/invitations/volume/plain.blade.php
+++ resources/views/emails/invitations/volume/plain.blade.php
@@ -4,9 +4,9 @@ I hope you’re having an excellent {{ $day }}.
 \r\n\r\n
 I’m reaching out today because I noticed your firm's website was a little out of date.
 \r\n\r\n
-Could I propose a redesign for your home page and show you what an updated site might look like? My plan is to use your existing site as a starting point for branding, but am also happy to include anything you might have input on. I definitely want to stay true to the reputation you've already built.
+Could I propose a redesign for your home page and show you what an updated site might look like? 
 \r\n\r\n
-Let me know, I'm always happy to help.
+My plan is to use your existing site as a starting point for branding, but I am also happy to include anything you might have input on. I definitely want to stay true to the reputation you've already built.
 \r\n\r\n
 Best regards,
 \r\n
